7fe027d8672d3538e98771a3ae11edc9dcaf492a
Sangheon <buru1020@gmail.com>
2023-07-31 21:11:29 +0900

    불필요 제거

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "First post!",
+        "excerpt":"첫번째 글 이제 성공 하냐?   System.out.println(\"HELLO World!!\");  ","categories": [],
+        "tags": [],
+        "url": "/first/",
+        "teaser": null
+      }]
