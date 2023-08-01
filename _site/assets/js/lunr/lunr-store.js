1216483eee05209a97bac7e7b89c24ab68551409
Sangheon <buru1020@gmail.com>
2023-08-01 21:07:24 +0900

    카테고리, 태그 추가

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
         "title": "First post!",
-        "excerpt":"첫번째 글 이제 성공 하냐?   System.out.println(\"HELLO World!!\");  ","categories": [],
-        "tags": [],
-        "url": "/first/",
+        "excerpt":"첫번째 글 이제 성공 하냐?   System.out.println(\"HELLO World!!\");  ","categories": ["환경설정"],
+        "tags": ["MacOS","Setting"],
+        "url": "/%ED%99%98%EA%B2%BD%EC%84%A4%EC%A0%95/first/",
         "teaser": null
       }]
