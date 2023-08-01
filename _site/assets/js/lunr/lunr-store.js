8d9c6eb9e5ac62eec876cb6433ba6c6ec0fc1836
Sangheon <buru1020@gmail.com>
2023-08-01 21:45:01 +0900

    404 page

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "First post!",
-        "excerpt":"첫번째 글 이제 성공 하냐?   System.out.println(\"HELLO World!!\");  ","categories": ["환경설정"],
+        "excerpt":"첫번째 글 이제 성공 하냐?   System.out.println(\"HELLO World!!\");  SECOND  오예 2-1  ","categories": ["환경설정"],
         "tags": ["MacOS","Setting"],
         "url": "/%ED%99%98%EA%B2%BD%EC%84%A4%EC%A0%95/first/",
         "teaser": null
